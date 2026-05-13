ddc07075abefa92836a06411102d9bab26ec23c2
komilazio <victordumbiri88@gmail.com>
2026-05-13 19:35:31 +0300

    adding .gitignore again

--- cc_echo/target/debug/cc_echo.d
+++
@@ -1,1 +0,0 @@
-/home/lazio/Workspace/projects/Becoming-rustacean/cc_echo/target/debug/cc_echo: /home/lazio/Workspace/projects/Becoming-rustacean/cc_echo/src/main.rs
--- cc_echo/target/debug/deps/cc_echo-77799329faaacbfa.d
+++
@@ -1,5 +0,0 @@
-/home/lazio/Workspace/projects/Becoming-rustacean/cc_echo/target/debug/deps/cc_echo-77799329faaacbfa.d: src/main.rs
-
-/home/lazio/Workspace/projects/Becoming-rustacean/cc_echo/target/debug/deps/cc_echo-77799329faaacbfa: src/main.rs
-
-src/main.rs:
